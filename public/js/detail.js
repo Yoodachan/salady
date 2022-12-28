e7e012dabf15d9874ee7129906292029635c0d38
YDC <miniming1026@naver.com>
2022-12-28 12:37:46 +0900

    제품 페이지 반응형 구현 테스트

--- public/js/detail.js
+++ public/js/detail.js
@@ -12,19 +12,19 @@ const cate_wrap = document.getElementsByClassName('cate_wrap')[0];
 
 function cate () {
 	if (q_cate == 'salady' ) {
-		title_img.style.background = "url(../images/list_title_img_001.png)";
+		title_img.style.background = "url(./images/list_title_img_001.png)";
 		detail_title.innerHTML = "누구나 맛있게 즐길 수 있도록 <br> 국내산 프리미엄 채소로만 구성된 샐러디"
 		cate_salady.classList.remove('cate_item')
 		cate_salady.classList.add('cate_select')
 	}
 	if (q_cate == 'warm_bowl' ) {
-		title_img.style.background = "url(../images/list_title_img_002.png)";
+		title_img.style.background = "url(./images/list_title_img_002.png)";
 		detail_title.innerHTML = "신선한 채소 베이스 위에 따끈한 통곡물 밥이 더해져 <br> 더욱 든든하게 즐길 수 있는 웜볼"
 		cate_warm.classList.remove('cate_item')
 		cate_warm.classList.add('cate_select')
 	}
 	if ( (q_cate == 'wrap') || (q_cate == 'sand')) {
-		title_img.style.background = "url(../images/list_title_img_003.png)";
+		title_img.style.background = "url(./images/list_title_img_003.png)";
 		detail_title.innerHTML = "또띠아로 돌돌 말아낸 맛있고 간편한 랩/웜랩과 <br> 씹을수록 고소한 귀리번이 매력적인 샌드"
 		if ( q_cate == 'wrap' ) {
 			cate_wrap.classList.remove('cate_item')
